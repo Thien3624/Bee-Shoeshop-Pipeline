e5bd995b609a0326884df4552350edda44f3be1d
Thiện <duythien3624@gmail.com>
2025-08-02 01:29:23 +0700

    feat(project): create base project

---
+++ bee-shoes-frontend/src/AppConfig.js
@@ -0,0 +1,11 @@
+/** @format */
+
+export const AppConfig = {
+  apiUrl: "http://192.168.48.33:8080",
+  routerBase: "",
+};
+
+export const AppConfigAddress = {
+  apiUrl: "",
+  routerBase: "",
+};
